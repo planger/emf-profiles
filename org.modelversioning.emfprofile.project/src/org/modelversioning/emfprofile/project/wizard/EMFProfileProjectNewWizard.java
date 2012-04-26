d5b4183d71d117fd31eb679a7d0b2ce25dd249fe
Philip Langer <p.langer@gmail.com>
2012-04-26 15:42:11 +0200

    Add new EMF Profile Wizard.

---
+++ org.modelversioning.emfprofile.project/src/org/modelversioning/emfprofile/project/wizard/EMFProfileProjectNewWizard.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2012 modelversioning.org
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.modelversioning.emfprofile.project.wizard;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+/**
+ * Wizard for creating new EMF Profile projects.
+ * 
+ * @author <a href="mailto:langer@big.tuwien.ac.at">Philip Langer</a>
+ *
+ */
+public class EMFProfileProjectNewWizard extends Wizard implements INewWizard {
+
+	public EMFProfileProjectNewWizard() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
